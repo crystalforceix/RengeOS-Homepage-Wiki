98c3fadb81dc7b139941e610bd37029b76b991b7
crystalforceix <crystalforceix@gmail.com>
2026-01-22 19:31:53 +0700

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import catppuccin from "@catppuccin/starlight";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'RengeOS docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/RengeOS/RengeOS-Homepage' }],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/RengeOS' }],
+			plugins: [
+        			catppuccin({
+					dark: { flavor: "mocha", accent: "mauve" },
+          				light: { flavor: "latte", accent: "mauve" },
+       				})
+      			],
+			logo: {
+        			src: './public/favicon.svg',
+      			},
+			favicon: './public/favicon.svg',
 			sidebar: [
 				{
 					label: 'Guides',
